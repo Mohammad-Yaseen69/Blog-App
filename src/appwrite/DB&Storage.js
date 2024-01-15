600828837d1622a7d9b3ff33fc6319222d495948
Mohammad Yaseen <yaseenyaseenali69@gmail.com>
2024-01-15 23:32:24 +0500

    Creating complete Database service

---
+++ src/appwrite/DB&Storage.js
@@ -0,0 +1,104 @@
+import config from "../config/config";
+import { Client, Databases, Query, Storage } from "appwrite";
+
+
+export class Service {
+    Client = new Client();
+    Database;
+    Storage;
+    constructor() {
+        this.Client
+            .setEndpoint(config.AppWrite_Url)
+            .setProject(config.AppWrite_Project_Id)
+
+        this.Database = new Databases(this.Client);
+        this.Storage = new Storage(this.Client);
+    }
+
+    async createPost({ Title, Content, slug, Img, Status, UserId }) {
+        try {
+            await this.Database.createDocument(
+                config.AppWrite_Database_Id,
+                config.AppWrite_Collection_Id,
+                slug,
+                {
+                    Title,
+                    Content,
+                    Img,
+                    Status,
+                    UserId
+                }
+            )
+            return true
+        } catch (error) {
+            return error
+        }
+    }
+
+    async updatePost(slug, { Title, Content, Img, Status, UserId }) {
+        try {
+            const response = await this.Database.updateDocument(
+                config.AppWrite_Database_Id,
+                config.AppWrite_Collection_Id,
+                slug,
+                {
+                    Title,
+                    Content,
+                    Img,
+                    Status,
+                }
+            )
+            return response
+        } catch (error) {
+            return error
+        }
+
+    }
+
+    async deletePost(slug) {
+        try {
+            const response = await this.Database.deleteDocument(
+                config.AppWrite_Database_Id,
+                config.AppWrite_Collection_Id,
+                slug,
+            )
+
+            return response
+        } catch (error) {
+            return error
+        }
+    }
+
+    async getPost(slug) {
+        try {
+            const response = await this.Database.getDocument(
+                config.AppWrite_Database_Id,
+                config.AppWrite_Collection_Id,
+                slug
+            )
+            return response
+        } catch (error) {
+            return error
+        }
+    }
+
+    async getAllPost(){
+        try {
+            const response = await this.Database.listDocuments(
+                config.AppWrite_Database_Id,
+                config.AppWrite_Bucket_Id,
+                [
+                    Query.equal('Status' , 'active')
+                ]
+            )
+
+            return response
+        } catch (error) {
+            return error
+        }
+    }
+}
+
+const service = new Service();
+
+export default service;
