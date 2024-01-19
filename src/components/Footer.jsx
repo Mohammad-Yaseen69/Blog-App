2e5649589fbb676f80741efc05b8dc496987edee
Mohammad Yaseen <yaseenyaseenali69@gmail.com>
2024-01-19 22:55:46 +0500

    Making component responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,14 +13,16 @@ const Footer = () => {
                         className='w-[100px] md:relative md:bottom-9 h-[100px] rounded-full'
                         alt="" />
 
-                    <div className='flex gap-14'>
+                    <div className='flex gap-14 flex-wrap'>
                         {footerLinks.map(link => (
                             <div className='flex  flex-col gap-3'>
-                                <h2 className='font-poppins font-bold text-[1.5vw]'>{link.title}</h2>
-                                <div className='flex flex-col gap-2'>
+                                <h2 className='font-poppins max-md:text-[2vw] max-ss:text-[3vw] max-md:text-center font-bold text-[1.5vw]'>{link.title}</h2>
+                                <div className='flex flex-col max-md:items-center gap-2'>
                                     {
                                         link.links.map(link => (
-                                            <p className='font-semibold font-mono text-[1vw] cursor-pointer text-gray-200'>{link.name}</p>
+                                            <p
+                                                className='font-semibold max-ss:text-[2.3vw] font-mono max-md:text-[1.5vw] text-[1vw] cursor-pointer
+                                                 text-gray-200'>{link.name}</p>
                                         ))
                                     }
                                 </div>
