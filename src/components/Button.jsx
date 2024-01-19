ad0a768a569da38244a7e8348805f637709851b5
Mohammad Yaseen <yaseenyaseenali69@gmail.com>
2024-01-19 19:33:39 +0500

    Creating Button Component

---
+++ src/components/Button.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Button = ({ callback, name }) => {
+    return (
+        <button
+            onClick={callback}
+            type="button"
+            className="px-6 py-3 focus:ring-offset-2 focus:ring-blue-500 text-white font-medium text-xs leading-tight uppercase shadow-md hover:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease rounded-md hover:bg-blue-800 bg-blue-600"
+        >
+            {name}
+        </button>
+    )
+}
+
+export default Button
