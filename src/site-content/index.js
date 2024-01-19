c2f36446e66a5a464c3c26efa238f5005ead81c8
Mohammad Yaseen <yaseenyaseenali69@gmail.com>
2024-01-19 20:24:00 +0500

    Creating a content file so it can be loop over and also customizable

---
+++ src/site-content/index.js
@@ -0,0 +1,63 @@
+const footer = [
+    {
+        title: 'Company',
+        links: [
+            {
+                name: 'Features',
+                link: 'feature'
+            },
+            {
+                name: 'Pricing',
+                link: 'pricing'
+            },
+            {
+                name: 'Affiliate Program',
+                link: 'affiliate'
+            },
+            {
+                name: 'Press Kit',
+                link: 'press-kit'
+            }
+        ]
+    }
+
+    ,
+    {
+        title: 'Support',
+        links: [
+            {
+                name: 'Account',
+                link: 'account'
+            },
+            {
+                name: 'Help',
+                link: 'help'
+            },
+            {
+                name: 'Customer Support',
+                link: 'customer-support'
+            },
+            {
+                name: 'Contact Us',
+                link: 'contact-us'
+            }
+        ]
+    }    
+    ,
+    {
+        title: 'Legal',
+        links: [
+            {
+                name: 'Terms and Condition',
+                link: 'terms-and-condition'
+            }
+            ,
+            {
+                name: 'Privacy Policy',
+                link: 'privacy-policy'
+            }
+        ]
+    }
+]
+
+export { footer }
