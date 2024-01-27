5918b2443237633740a0e430c58f6403c0c443fa
Mohammad Yaseen <yaseenyaseenali69@gmail.com>
2024-01-27 17:29:53 +0500

    Making PostCard css better

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -5,16 +5,22 @@ import service from '../appwrite/DB&Storage'
 const PostCard = ({ $id, Img, Title, Content }) => {
     return (
         <Link to={`/post/${$id}`}>
-            <div className='w-full bg-gray-100 rounded-xl p-4'>
-                <div className='w-full justify-center mb-4'>
-                    <img src={service.getFilePreview(Img)} alt={Title}
-                        className='rounded-xl' />
+            <div className='w-full bg-white rounded-xl shadow-md p-4 transition-transform transform hover:scale-105'>
+                <div className='w-full mb-4'>
+                    <img
+                        src={service.getFilePreview(Img)}
+                        alt={Title}
+                        className='w-full h-32 text-black object-cover rounded-xl'
+                    />
                 </div>
-                <h2
-                    className='text-xl font-bold'
-                >{Title}</h2>
+                <h2 className='text-3xl text-center text-black font-bold'>{Title}</h2>
+                <p>
+                    {Content.substring(0, 20)}...
+                    <span className='text-blue-500 font-bold'>Read More</span>
+
+                </p>
             </div>
-        </Link>
+        </Link >
     )
 }
 
