03636acb17392f27f703400bc3a0f7da6ad2409c
Mohammad Yaseen <yaseenyaseenali69@gmail.com>
2024-01-27 19:55:53 +0500

    Adding Html react parser

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import service from '../appwrite/DB&Storage'
+import HtmlParser from 'react-html-parser'
 
 const PostCard = ({ $id, Img, Title, Content }) => {
-    const contentWithoutPTag = Content.replace(/<\/?p>/g, '');
+
     return (
         <Link to={`/post/${$id}`}>
             <div className='max-w-full min-h-[50vh] flex flex-col justify-center items-center bg-blue-200 rounded-xl shadow-md p-4 transition-transform transform hover:scale-[1.010]'>
@@ -16,8 +17,8 @@ const PostCard = ({ $id, Img, Title, Content }) => {
                 </div>
                 <h2 className='text-3xl text-center text-black font-bold'>{Title}</h2>
                 <div className='text-black mt-5 text-center min-w-full text-bold overflow-hidden'>
-                    <p className='inline-block'>{contentWithoutPTag.substring(0, 30)}...</p>
-                    <span className='text-blue-500 font-bold'>Read More</span>
+                    {HtmlParser(Content.substring(0, 70))}
+                   {Content.length > 70 && <span className='text-blue-500 font-bold'> Read More</span>}
                 </div>
             </div>
 
