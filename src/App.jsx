74270205a16d71a5595d82e05837f3901a2a5000
Mohammad Yaseen <yaseenyaseenali69@gmail.com>
2024-01-27 19:08:13 +0500

    Making Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import service from "./appwrite/DB&Storage";
 import { useDispatch, useSelector } from "react-redux";
 import { login, logout, toggleloading } from "./store/authSlice";
 import { Footer, Header, Login } from "./components/";
-import { Outlet } from "react-router-dom";
+import { Outlet, Route } from "react-router-dom";
 
 function App() {
   const data = useSelector(state => state.auth.user)
@@ -37,42 +37,8 @@ function App() {
     <div className="w-full h-screen relative  text-white">
 
       <Header />
-      {/* {loading ? <h1>Loading...</h1> :
 
-        <div>
-          {status ? (
-            // If the user is logged in
-            <div>
-              <h1>{data.userData.name}</h1>
-              <Button name="logout" callback={() => {
-                auth.logout()
-                dispatch(logout())
-                window.location.reload()
-              }} />
-              <img src="../assest/logo" className="w-[100px]" alt="" />
-            </div>
-          ) :
-            // If the user is not logged in
-            (
-              <Button
-                name="login"
-                callback={() => {
-                  auth.login({
-                    email: "yaseen@gmail.com",
-                    password: "test12345"
-                  }).then((data) => {
-                    dispatch(login({ userData: data }))
-                    window.location.reload()
-                  })
-                }} />
-            )}
-        </div>
-      } */}
       <Outlet />
-
-      {status && <h1>Welcome {data.userData?.name}</h1>}
-
-      <div className="h-[100vh]  w-full "></div>
 
       <Footer />
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate, Link } from 'react-router-dom'
+import { Button } from '../components'
+
+
+const Home = () => {
+    const status = useSelector(state => state.auth.status)
+    const navigate = useNavigate()
+    const data = useSelector(state => state.auth.user)
+    return (
+
+        <div className='w-full h-screen flex items-center  justify-center relative bottom-2'>
+            {status ?
+                <div className='w-[70%] mb-40 items-center flex flex-col gap-7'>
+                    <h1 className='font-bold text-[3vw]'>Welcome <span className='text-green-700'>{data.userData?.name}</span></h1>
+                    <p className='text-center w-[70%] font-semibold'>This is a web application designed for users to create, read, and manage blog posts. It includes authentication features for secure access and additional functionalities to enhance the blogging experience.</p>
+                    <div className='flex gap-4'>
+                        <Button name="See Posts" callback={() => navigate("/posts")} />
+                        <Button name="Create Post" callback={() => navigate("/Create-Post")}></Button>
+                    </div>
+                </div>
+                :
+                <div className='w-[70%] mb-40 items-center flex flex-col gap-7'>
+                    <h1 className='font-bold text-[3vw]'>Welcome</h1>
+                    <p className='text-center w-[70%]  font-semibold'>This is a web application designed for users to create, read, and manage blog posts. It includes authentication features for secure access and additional functionalities to enhance the blogging experience.</p>
+                   <p className='text-red-500 font-bold'>Login to Read and Create Posts</p>
+                </div>}
+        </div>
+    )
+}
+
+export default Home
